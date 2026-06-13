5b2648b12583185e107b2ac6da0c27ca942dd173
zzrove <58414473+zzrove@users.noreply.github.com>
2026-06-13 18:11:14 +0800

    Update build.sh
    Signed-off-by: zzrove <58414473+zzrove@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -14,6 +14,9 @@ fi
 
 BASE_PATH=$(cd "$WRT_CORE_PATH" && pwd)
 
+# 确保加载了包含插件管理函数的模块
+source "$BASE_PATH/modules/packages.sh"
+
 Dev=$1
 Build_Mod=$2
 
@@ -201,39 +204,4 @@ if [[ -d action_build ]]; then
     BUILD_DIR="action_build"
 fi
 
-"$BASE_PATH/update.sh" "$REPO_URL" "$REPO_BRANCH" "$BUILD_DIR" "$COMMIT_HASH"
-
-apply_config
-remove_uhttpd_dependency
-
-cd "$BASE_PATH/../$BUILD_DIR"
-make defconfig
-
-if grep -qE "^CONFIG_TARGET_x86_64=y" "$CONFIG_FILE"; then
-    DISTFEEDS_PATH="$BASE_PATH/../$BUILD_DIR/package/emortal/default-settings/files/99-distfeeds.conf"
-    if [ -d "${DISTFEEDS_PATH%/*}" ] && [ -f "$DISTFEEDS_PATH" ]; then
-        sed -i 's/aarch64_cortex-a53/x86_64/g' "$DISTFEEDS_PATH"
-    fi
-fi
-
-if [[ $Build_Mod == "debug" ]]; then
-    exit 0
-fi
-
-TARGET_DIR="$BASE_PATH/../$BUILD_DIR/bin/targets"
-if [[ -d $TARGET_DIR ]]; then
-    find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" -o -name "*efi.img.gz" -o -name "*.itb" -o -name "*.fip" -o -name "*.ubi" -o -name "*rootfs.tar.gz" \) -exec rm -f {} +
-fi
-
-make download -j$(($(nproc) * 2))
-make -j$(($(nproc) + 1)) || make -j1 V=s
-
-FIRMWARE_DIR="$BASE_PATH/../firmware"
-\rm -rf "$FIRMWARE_DIR"
-mkdir -p "$FIRMWARE_DIR"
-find "$TARGET_DIR" -type f \( -name "*.bin" -o -name "*.manifest" -o -name "*efi.img.gz" -o -name "*.itb" -o -name "*.fip" -o -name "*.ubi" -o -name "*rootfs.tar.gz" \) -exec cp -f {} "$FIRMWARE_DIR/" \;
-\rm -f "$BASE_PATH/../firmware/Packages.manifest" 2>/dev/null
-
-if [[ -d action_build ]]; then
-    make clean
-fi
+"$BASE_PATH/update.sh" "$REPO_URL" "$REPO_BRANCH"
